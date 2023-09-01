e60e21245f3817652c48c0c65164179260ce5661
Aman Verma <amanverma03328@gmail.com>
2023-09-01 10:44:43 +0530

    initial commit

---
+++ functionality.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+    // global variables
+    let input_box = $("#text-to-convert-input");
+    let output_box = $("#text-to-convert-output");
+    let convert_button = $("#convert-button"); 
+
+    // event binds
+    convert_button.click(function(){
+        let text_to_convert = input_box.val();
+        let indent_level = 0;
+
+        // console.log(text_to_convert); 
+        for( let character of text_to_convert){
+            // console.log(character);
+            // string.repeat(count);
+            if(){
+
+            }else if(){
+
+            }else{
+
+            }
+        }
+
+
+
+    });
+    
+});
