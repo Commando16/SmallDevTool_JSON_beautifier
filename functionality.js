ede17e12016c386ef840d56b477edf47365f39ab
Aman Verma <amanverma03328@gmail.com>
2023-09-02 19:09:14 +0530

    feature: convert logic implemented

--- functionality.js
+++ functionality.js
@@ -8,22 +8,46 @@ $( document ).ready(function() {
     convert_button.click(function(){
         let text_to_convert = input_box.val();
         let indent_level = 0;
+        let converted_text = "";
+        let comma_line_breaker = "";
 
         // console.log(text_to_convert); 
         for( let character of text_to_convert){
             // console.log(character);
             // string.repeat(count);
-            if(){
-
-            }else if(){
-
-            }else{
-
+            if(character=="[" || character=="{"){
+                let indent = " ".repeat(4*indent_level);
+                converted_text+=indent+character+"\n";
+                indent_level+=1;
+                
+                indent = " ".repeat(4*indent_level);
+                converted_text+=indent;
+            }
+            else if(character=="]" || character=="}"){
+                indent_level-=1;
+                let indent = " ".repeat(4*indent_level);
+                converted_text+="\n"+indent+character+"\n";
+            }
+            else if(character==","){
+                comma_line_breaker+=character;
+                converted_text+character;
+            }
+            else if(comma_line_breaker=="," && character==" "){
+                comma_line_breaker+=character;
+            }
+            else if(comma_line_breaker==", "){
+                if(character=="\'"){
+                    let indent = " ".repeat(4*indent_level);
+                    converted_text+=", \n"+indent+"\'";
+                }
+                comma_line_breaker="";
+            }
+            else{
+                converted_text+=character;
             }
         }
 
-
-
+        // outputing the converted text
+        output_box.text(converted_text);
     });
-    
 });
